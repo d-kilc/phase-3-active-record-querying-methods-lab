f366f6d807ef382bf8bf400d4ae592504bf81317
Daniel <dkilcullen25@gmail.com>
2022-02-04 15:43:50 -0600

    commit tree da9ff0ed3fbdb2b80a7c85668779b1b8a3535f7f

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+puts "Seeding!"
+
+Show.create name: "jimmy neutron",network: "Nick", day: "Monday", rating:9, season:'2'
+Show.create name: "Spongebob",network: "Nick", day: "Monday", rating:10, season:'2'
+Show.create name: "fairly odd",network: "Nick", day: "Monday", rating:9, season:'2'
+Show.create name: "danny phantom",network: "Nick", day: "Monday", rating:6, season:'2'
+Show.create name: "hey arnold",network: "Nick", day: "Monday", rating:8, season:'2'
+
+puts "Finished seeding."
